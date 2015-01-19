10bd688a8bc7790933973d1010f2c875d7d3e96c
Chris Harrison <dev@ravngr.com>
2015-01-19 14:23:38 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# usleepstop
-µSleepStop prevents a Windows machine from sleeping while the app is running, while maintaining a very small footprint.
+# µSleepStop
+The program does exactly two things:
+1. Make a call to SetThreadExecutionState with ES_DISPLAY_REQUIRED set
+2. Open a message box
+
+The effect being that as long as that message box is open, Windows will not let the computer go to sleep (or go to screensaver). This is the same mechanism used by many media players to stop the screen turning off while media is playing.
+
+If anybody has any ideas as to how to make the resulting executable smaller then let me know.
---
+++ usleepstop.asm
@@ -0,0 +1,27 @@
+.386
+.model flat, stdcall
+option casemap :none
+
+extrn SetThreadExecutionState@4 : PROC
+extrn MessageBoxA@16 : PROC
+extrn ExitProcess@4 : PROC
+
+.data
+        MsgText db "Preventing sleep. Press OK to exit.", 0
+        MsgTitle db "uSleepStop", 0
+
+.code
+start:
+        push -2147483646                ; ES_CONTINUOUS | ES_DISPLAY_REQUIRED
+        call SetThreadExecutionState@4
+        push 48							; MB_ICONWARNING
+        lea eax, MsgTitle
+        push eax
+        lea ebx, MsgText
+        push ebx
+        push 0
+        call MessageBoxA@16
+        push ebx
+        call ExitProcess@4
+
+end start
